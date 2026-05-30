8995d2803c766b5697a24a6cc4f21cdecacd1df7
akash chaudhary <cruzerboy8@gmail.com>
2026-05-31 02:49:16 +0545

    Day 14 added

--- Day 14 SQL/DQL.sql
+++ Day 14 SQL/DQL.sql
@@ -14,3 +14,16 @@ insert into student( rollno,name) values(101,'ashika'),
 select * from student;
 
 insert into student values(105,'dev');
+
+create database xyz;
+use xyz;
+create table employee (
+	id int primary key,
+    name varchar(50),
+    salary int);
+insert into employee(id,name,salary) 
+values
+(1,"adam",25000),
+(2,"bob",30000),
+(3,"casey",40000);
+select * from employee;
