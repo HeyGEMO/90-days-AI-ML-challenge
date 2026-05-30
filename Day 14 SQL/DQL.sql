b382cc0310c8e49e5a38ef52d3d3f9d9bf438159
akash chaudhary <cruzerboy8@gmail.com>
2026-05-31 03:22:01 +0545

    Day 14 added

--- Day 14 SQL/DQL.sql
+++ Day 14 SQL/DQL.sql
@@ -39,3 +39,21 @@ id int,
 salary int default 25000);
 insert into emp(id) values (1);
 select * from emp;
+
+drop database college;
+create database college;
+use college;
+create table student(
+rollno int primary key,
+name varchar(50),
+marks int not null,
+grade varchar(1),
+city varchar(20));
+insert into student(rollno,name,marks,grade,city) values
+(101,'hari',87,'B','butwal'),
+(102,'prazwol',90,'A','ktm'),
+(103,'rikesh',95,'A','ktm'),
+(104,'bibek',85,'B','beni'),
+(105,'dipin',92,'A','ktm'),
+(106,'prazwol',82,'B','ktm');
+select * from student;
