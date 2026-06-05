be73a33500bbc8a7a92ff2052c9ce82fc5d6fca4
akash chaudhary <cruzerboy8@gmail.com>
2026-06-05 22:32:55 +0545

    Day 17 completed

--- Day 17 SQL/sub queries.sql
+++ Day 17 SQL/sub queries.sql
@@ -1,1 +1,27 @@
+#SQL sub queries -> inner queries -> nested queries
 use college;
+
+select avg(marks) from student;
+
+select full_name,marks 
+from student 
+where marks > (select avg(marks) from student);
+
+select id from student where (id%2)=0;
+
+select full_name,id from student where id%2=0;
+#or
+select full_name,id from student where id in (select id from student where (id%2)=0);
+
+#using from
+select * from student where city='ktm';
+select max(marks) from (select * from student where city='ktm') as temp;
+
+#in select
+select (select max(marks) from student),full_name from student;
+
+#views
+create view view1 as select id,full_name,marks from student;
+select * from view1;
+select * from view1 where marks>90;
+drop view view1;
